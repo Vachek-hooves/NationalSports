abc945dd514fd4c36ab11a379c09a980a6373840
Vachek-hooves <vachek0602@gmail.com>
2024-08-25 23:48:10 +0300

    DRY for quiz btns

--- components/Quiz/index.js
+++ components/Quiz/index.js
@@ -1,7 +1,4 @@
 import QuizQuestion from './QuizQuestion';
 import QuizOptions from './QuizOptions';
-import NextBtn from './NextBtn';
-import ResultsBtn from './ResultsBtn';
-import RestartBtn from './RestartBtn';
 
-export {QuizOptions, QuizQuestion, NextBtn, ResultsBtn, RestartBtn};
+export {QuizOptions, QuizQuestion};
